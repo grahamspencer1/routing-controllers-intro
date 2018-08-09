a9497e731bbcb15d9223acd0be3ab8027aa9aaba
Graham Spencer <grahamspence@gmail.com>
2018-08-08 22:44:26 -0400

    added headers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,15 @@
 class PagesController < ApplicationController
 
   def welcome
+    @header = "Hello! So good of you to join us."
   end
 
   def about
+    @header = "This app was built by me, Graham Spencer."
   end
 
   def contest
+    @header = "Contest cancelled due to pending lawsuits."
   end
 
 end
